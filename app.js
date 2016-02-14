de4f85669dd690b8659fa0f9bf48382b71eb95b4
Randall Crock <rlcrock@gmail.com>
2016-02-14 14:48:20 -0700

    Adding ejs routes for views

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var routes = require('./routes/index');
+
 module.exports = app; // for testing
 
 var config = {
@@ -11,13 +17,22 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  // register swagger-ui
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+
+  // add non-api routes
+  app.use('/', routes);
+
+  // add static hosting
+  app.use(express.static(path.join(__dirname, 'assets')));
+
   // install middleware
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
 });
